1de617f3c6302d98bb7320410455a730731ed86b
Bhone Pyae Kyaw <137211983+BhonePyae-Kyaw@users.noreply.github.com>
2025-10-05 01:29:59 +0700

    feat: handle  prerender routes

--- src/app/app.routes.server.ts
+++ src/app/app.routes.server.ts
@@ -2,7 +2,20 @@ import { RenderMode, ServerRoute } from '@angular/ssr';
 
 export const serverRoutes: ServerRoute[] = [
   {
+    path: '',
+    renderMode: RenderMode.Prerender,
+  },
+  {
+    path: 'details/:id',
+    renderMode: RenderMode.Prerender,
+    getPrerenderParams: async () => {
+      // Return all the valid housing location IDs for prerendering
+      // Based on the housing service data, IDs are 0-9
+      return Array.from({ length: 10 }, (_, i) => ({ id: i.toString() }));
+    },
+  },
+  {
     path: '**',
-    renderMode: RenderMode.Prerender
-  }
+    renderMode: RenderMode.Prerender,
+  },
 ];
