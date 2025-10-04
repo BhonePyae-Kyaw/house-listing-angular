27a7630e4b3372397afb64dae909fdd00cd99920
Bhone Pyae Kyaw <137211983+BhonePyae-Kyaw@users.noreply.github.com>
2025-10-04 22:16:31 +0700

    feat: route parameters, routerLink, specific housing location info

--- src/app/details/details.ts
+++ src/app/details/details.ts
@@ -1,11 +1,19 @@
-import { Component } from '@angular/core';
-
+import { Component, inject } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { HousingService } from '../housing';
+import { HousingLocationInfo } from '../housinglocation';
 @Component({
   selector: 'app-details',
   imports: [],
   templateUrl: './details.html',
-  styleUrl: './details.css'
+  styleUrl: './details.css',
 })
 export class Details {
-
+  route: ActivatedRoute = inject(ActivatedRoute);
+  housingService = inject(HousingService);
+  housingLocation: HousingLocationInfo | undefined;
+  constructor() {
+    const housingLocationId = Number(this.route.snapshot.params['id']);
+    this.housingLocation = this.housingService.getHousingLocationById(housingLocationId);
+  }
 }
